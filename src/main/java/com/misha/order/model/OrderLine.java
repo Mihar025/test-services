0f34b4e4e63a12577025e1e38c5f694736dbb5d4
Mishar025 <mishamay583@gmail.com>
2024-08-25 13:55:23 -0400

    Added customer microservice

---
+++ src/main/java/com/misha/order/model/OrderLine.java
@@ -0,0 +1,32 @@
+package com.misha.order.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+@Setter
+@Entity
+public class OrderLine {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private Order order;
+
+    private Integer productId;
+
+    private Double quantity;
+
+
+
+
+
+
+
+}
