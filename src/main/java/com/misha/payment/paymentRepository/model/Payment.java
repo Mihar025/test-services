88eb1866e5c837e5b29ccc0e8224e43e16544cac
Mishar025 <mishamay583@gmail.com>
2024-09-02 19:07:17 -0400

    Added customer microservice

---
+++ src/main/java/com/misha/payment/paymentRepository/model/Payment.java
@@ -0,0 +1,39 @@
+package com.misha.payment.paymentRepository.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+@Setter
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+@Table(name = "payment")
+public class Payment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    private BigDecimal amount;
+    @Enumerated(EnumType.STRING)
+    private PaymentMethod paymentMethod;
+    private Integer orderId;
+    @CreatedDate
+    @Column(updatable = false, nullable = false)
+    private LocalDateTime created_at;
+    @LastModifiedDate
+    @Column(nullable = false, insertable = false)
+    private LocalDateTime lastModifiedDate;
+}
+
+
+
+
+
