2e57965ecfc4d5731f3a0de4a1e0c550f99b4989
Mishar025 <mishamay583@gmail.com>
2024-09-02 19:08:43 -0400

    Added customer microservice

---
+++ src/main/java/com/misha/payment/paymentService/PaymentService.java
@@ -0,0 +1,36 @@
+package com.misha.payment.paymentService;
+
+import com.misha.payment.mapper.PaymentMapper;
+import com.misha.payment.notification.NotificationProducer;
+import com.misha.payment.paymentRepository.PaymentRepository;
+import com.misha.payment.paymentRequests.PaymentRequest;
+import com.misha.payment.requests.PaymentNotificationRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class PaymentService {
+
+    private final PaymentRepository paymentRepository;
+    private final PaymentMapper paymentMapper;
+    private final NotificationProducer notificationProducer;
+
+    public Integer createPayment(PaymentRequest request) {
+
+        var payment = paymentRepository.save(paymentMapper.toPayment(request));
+        notificationProducer.sendNotification(
+                new PaymentNotificationRequest(
+                        request.orderReference(),
+                        request.amount(),
+                        request.paymentMethod(),
+                        request.customer().firstName(),
+                        request.customer().lastName(),
+                        request.customer().email()
+                )
+        );
+        return payment.getId();
+    }
+
+
+}
