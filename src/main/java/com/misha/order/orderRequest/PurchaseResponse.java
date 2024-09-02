47785af6c0a2e8cc565544dc828fae5da207f8ae
Mishar025 <mishamay583@gmail.com>
2024-09-02 19:08:54 -0400

    Added customer microservice

---
+++ src/main/java/com/misha/order/orderRequest/PurchaseResponse.java
@@ -0,0 +1,12 @@
+package com.misha.order.orderRequest;
+
+import java.math.BigDecimal;
+
+public record PurchaseResponse(
+        Integer productId,
+        String name,
+        String description,
+        BigDecimal price,
+        double quantity
+) {
+}
